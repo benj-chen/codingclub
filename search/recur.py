a04f737c78dece319bc8be82bf01d0831385665a
Benjamin Chen <51394608+benj-chen@users.noreply.github.com>
2020-10-15 15:31:07 -0700

    Create recur.py

---
+++ search/recur.py
@@ -0,0 +1,25 @@
+# Returns index of x in arr if present, else -1 
+def binary_Search(arr, l, r, x): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = l + (r - l) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+          
+        # If element is smaller than mid, then it  
+        # can only be present in left subarray 
+        elif arr[mid] > x: 
+            return binary_Search(arr, l, mid-1, x) 
+  
+        # Else the element can only be present  
+        # in right subarray 
+        else: 
+            return binary_Search(arr, mid + 1, r, x) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
